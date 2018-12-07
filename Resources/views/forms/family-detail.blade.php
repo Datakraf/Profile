b77a3a74158199193d405ae46eda7753e07985ab
Uzzair Baharudin <uzzaircode@gmail.com>
2018-12-07 20:18:46 +0800

    m

---
+++ Resources/views/forms/family-detail.blade.php
@@ -0,0 +1,33 @@
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <table id="myTable" class="table order-list table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Gmail</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="">
+                            <input type="text" name="name" class="form-control" />
+                        </td>
+                        <td class="">
+                            <input type="mail" name="mail" class="form-control" />
+                        </td>
+                        <td class="">
+                            <input type="text" name="phone" class="form-control" />
+                        </td>
+                        <td class=""><a class="deleteRow">
+                                <input type="button" class="btn btn-block " id="addrow" value="Add Row" />
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>                
+            </table>
+        </div>
+    </div>
+</div>
--- Resources/views/forms/personal-information.blade.php
+++ Resources/views/forms/personal-information.blade.php
@@ -3,13 +3,61 @@
         <li class="nav-item">
             <a class="nav-link active show" data-toggle="tab" href="#personal-detail">Personal Information</a>
         </li>
+        <li><a class="nav-link" data-toggle="tab" href="#family-detail">Family Information</a></li>
     </ul>
     <div class="tab-content">
         <div class="tab-pane fade active show" id="personal-detail">
             @include('profile::forms.personal-detail')
         </div>
-        <div class="tab-pane fade">
-
+        <div class="tab-pane fade" id="family-detail">
+            @include('profile::forms.family-detail')
         </div>
     </div>
 </form>
+
+@section('page-js')
+<script>
+    $(document).ready(function () {
+        var counter = 0;
+
+        $("#addrow").on("click", function () {
+            var newRow = $("<tr>");
+            var cols = "";
+
+            cols += '<td><input type="text" class="form-control" name="name' + counter + '" /></td>';
+            cols += '<td><input type="text" class="form-control" name="mail' + counter + '" /></td>';
+            cols += '<td><input type="text" class="form-control" name="phone' + counter + '" /></td>';
+
+            cols +=
+                '<td><input type="button" class="ibtnDel btn btn-md btn-danger " value="Delete"></td>';
+            newRow.append(cols);
+            $("table.order-list").append(newRow);
+            counter++;
+        });
+
+
+
+        $("table.order-list").on("click", ".ibtnDel", function (event) {
+            $(this).closest("tr").remove();
+            counter -= 1
+        });
+
+
+    });
+
+    function calculateRow(row) {
+        var price = +row.find('input[name^="price"]').val();
+
+    }
+
+    function calculateGrandTotal() {
+        var grandTotal = 0;
+        $("table.order-list").find('input[name^="price"]').each(function () {
+            grandTotal += +$(this).val();
+        });
+        $("#grandtotal").text(grandTotal.toFixed(2));
+    }
+
+</script>
+
+@endsection

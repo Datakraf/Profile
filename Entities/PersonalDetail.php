a562f84e2dba88af868727182e95917189ac16d5
Uzzair Baharudin <uzzaircode@gmail.com>
2018-12-19 13:05:44 +0800

    m

--- Entities/PersonalDetail.php
+++ Entities/PersonalDetail.php
@@ -5,6 +5,7 @@ namespace Modules\Profile\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Datakraf\User;
 use Modules\Profile\Events\PersonalDetailCreated;
+use Modules\Profile\Entities\Position;
 
 class PersonalDetail extends Model
 {
@@ -19,5 +20,9 @@ class PersonalDetail extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function position(){
+        return $this->belongsTo(Position::class,'position_id');
     }
 }
